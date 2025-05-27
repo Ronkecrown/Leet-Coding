f9ddd5d7e76ac967f3ed7b3b56ac553ab396789b
Aderonke Ajefolakemi <aderonkealagbada@gmail.com>
2025-05-27 16:21:43 +0000

    twoSum

--- LeetCode.py
+++ LeetCode.py
@@ -4,13 +4,13 @@ LeetCode.py contain
 
 
 class LeetCode:
-    def isPalindrome(self, x: int) -> bool:
+    def isPalindrome(self, x: int) -> bool:  # task 9
         x = str(x)
         if x[::-1] == x:
             return True
         return False
 
-    def twoSum(self, nums: list, target: int) -> list:
+    def twoSum(self, nums: list, target: int) -> list:  # task 1
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
